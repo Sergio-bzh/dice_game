f5136ddbe5a651bb3b81cbfd4b47b4e45371dfd7
sergio-nunez <sergio.nunez@laposte.net>
2024-03-08 11:08:43 +0100

    Logic: variables declared

---
+++ script.js
@@ -0,0 +1,19 @@
+/** START Action variables */
+const newGame = document.getElementById('newGame');
+const rollDice = document.querySelector('.roll-dice');
+const hold = document.querySelector('.hold');
+/** END Action variables */
+
+/** START display variables */
+let dice = document.getElementById('dice');
+let globalScore1 = document.getElementById('globalScore1');
+let globalScore2 = document.getElementById('globalScore2');
+let roundScore1 = document.getElementById('roundScore1');
+let roundScore2 = document.getElementById('roundScore2');
+/** END display variables */
+
+/** START alternatively "active" variables */
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
+/** END alternatively "active" variables */
+
