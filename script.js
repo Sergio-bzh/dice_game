b6544564f629201b59faa8ecba3a4d488142f277
sergio-nunez <sergio.nunez@laposte.net>
2024-03-08 15:00:23 +0100

    Logic: random number's generator function, dice faces array and roll-dice event listener added

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const hold = document.querySelector('.hold');
 /** END Action variables */
 
 /** START display variables */
-let dice = document.getElementById('dice');
+let dice = document.querySelector('img');
 let globalScore1 = document.getElementById('globalScore1');
 let globalScore2 = document.getElementById('globalScore2');
 let roundScore1 = document.getElementById('roundScore1');
@@ -17,3 +17,36 @@ const player1 = document.getElementById('player1');
 const player2 = document.getElementById('player2');
 /** END alternatively "active" variables */
 
+/** START storage and other needed variables */
+let randomNumber = 0;
+const diceFaces = [
+    'assets/images/one.jpg', 'assets/images/two.jpg','assets/images/three.jpg',
+    'assets/images/four.jpg','assets/images/five.jpg', 'assets/images/six.jpg'
+    ];
+/** END storage and other needed variables */
+
+
+/** START event listeners*/
+rollDice.addEventListener('click', function() {
+    randomNumber = generateRandomInt();
+    diceFaces.forEach(function (face, index) {
+        if(randomNumber == index){
+            dice.src = face;
+        }
+    });
+    return randomNumber;
+});
+console.log(randomNumber);
+/** END event listeners*/
+
+
+/** START DOM modification */
+
+/** END DOM modification */
+
+/** Random number generator */
+function generateRandomInt(min = 0, max = 6) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
