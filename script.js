a3f7f4de15f30fac78b12494259a4fb1b043f72b
sergio-nunez <sergio.nunez@laposte.net>
2024-03-09 03:19:18 +0100

    Logic: added functions to swap users and reset current counter

--- script.js
+++ script.js
@@ -23,38 +23,25 @@ const diceFaces = [
     'assets/images/one.jpg', 'assets/images/two.jpg','assets/images/three.jpg',
     'assets/images/four.jpg','assets/images/five.jpg', 'assets/images/six.jpg'
 ];
+let currentCounter = 0;
 const defaultImage = diceFaces[0];
 let newGamePoint = 'Player 1 <i class="fa-solid fa-circle fs-6 text-center"></i>';
-let activePoint  = 'Player 2 <i class="fa-solid fa-circle fs-6 text-center"></i>';
+let activePoint  = '<i class="fa-solid fa-circle fs-6 text-center"></i> Player 2';
 /** END storage and other needed variables */
 
 /** START event listeners*/
-newGame.addEventListener('click', resetParameters)
-
-rollDice.addEventListener('click', () => {
-    randomNumber = generateRandomInt();
-    diceFaces.forEach(function (face, index) {
-        if(randomNumber == index){
-            dice.src = face;
-        }
-    });
-    return randomNumber;
-});
-console.log(randomNumber);
+newGame.addEventListener('click', resetAllParameters);
+rollDice.addEventListener('click', swapActivePlayer);
 /** END event listeners*/
 
-/** START DOM modification */
-
-/** END DOM modification */
-
-/** Random number generator */
+/** START functions */
 function generateRandomInt(min = 0, max = 6) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function resetParameters(){
+function resetAllParameters(){
     dice.src = defaultImage;
     globalScore1.innerText  = 0;
     globalScore2.innerText  = 0;
@@ -63,4 +50,51 @@ function resetParameters(){
     player1.innerHTML = newGamePoint;
     player1.classList.add('fw-bolder');
     player2.classList.remove('fw-bolder');
+};
+
+function resetRoundCounter(){
+    roundScore1.innerText   = 0;
+    roundScore2.innerText   = 0;
+};
+
+function activePlayer2(){
+    if(randomNumber + 1 == 1 || globalScore1 >= 100){
+        resetRoundCounter();
+        player1.classList.remove('fw-bolder');
+        player2.classList.add('fw-bolder');
+        player1.innerHTML = 'Player 1'
+        player2.innerHTML = activePoint;
+    }
 }
+
+function activePlayer1(){
+    if (randomNumber + 1 == 1 || globalScore1 >= 100 && player2.classList.contains('fw-bolder')){
+        resetRoundCounter();
+        player1.classList.add('fw-bolder');
+        player2.classList.remove('fw-bolder');
+        player2.innerHTML = 'Player 2';
+        player1.innerHTML = newGamePoint;
+    }
+}
+
+function swapActivePlayer(){
+    randomNumber = generateRandomInt();
+    diceFaces.forEach((face, index) => {
+        if(randomNumber === index){
+            dice.src = face;
+            if(player1.classList.contains('fw-bolder')){
+                roundScore1.textContent = parseInt(roundScore1.textContent) + randomNumber + 1;
+                if(randomNumber + 1 == 1 || globalScore1 >= 100){
+                    activePlayer2();
+                }
+            } else if(player2.classList.contains('fw-bolder')) {
+                roundScore2.textContent = parseInt(roundScore2.textContent) + randomNumber + 1;
+                if(randomNumber + 1 == 1 || globalScore1 >= 100){
+                    activePlayer1();
+                }
+            }
+        };
+    });
+    return randomNumber;
+}
+/** END functions */
